0420e6da90b2199d4f7a4a9e1e0c060277c30fec
newjordan <newjordan@gmail.com>
2025-03-02 13:42:01 -0600

    removed defunct ui button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Sacred Grid controls', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const controlsHeading = screen.getByText(/Sacred Grid Controls/i);
+  expect(controlsHeading).toBeInTheDocument();
 });
